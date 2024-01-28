914ebfcb33fbcdcde95c3ff7778616d70ed6236f
ToGiFlip <filipe.gui.97@gmail.com>
2024-01-28 17:38:52 +0100

    Tweaked Explosion Scene

--- Assets/Code/PlayVFX.cs
+++ Assets/Code/PlayVFX.cs
@@ -6,14 +6,24 @@ public class PlayVFX : MonoBehaviour
 {
     [SerializeField] private VFXPlayer _animToPlay;
     [SerializeField] private float _delay;
+    [SerializeField] private AudioClip _soundToPlay;
 
-    [SerializeField] private bool _loopAnimation;
+
+
+
+    private  AudioSource _sfxPlayer;
+
+    void Awake()
+    {
+        _sfxPlayer = GetComponent<AudioSource>();
+    }
 
     // Start is called before the first frame update
     private IEnumerator Start()
     {
         yield return new WaitForSeconds(_delay);
         _animToPlay.PlayAnimation();
+        _sfxPlayer.PlayOneShot(_soundToPlay);
     }
 
 }
